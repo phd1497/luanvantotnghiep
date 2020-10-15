893729632f5ef105264c15dbc695e86255f58750
HOANG DAT <phd14971997@gmail.com>
2020-10-15 08:39:37 +0700

    page admin

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/','HomeController@index');
 Route::get('/trang-chu', 'HomeController@index');
+
+Route::get('/admin', 'AdminController@index');
